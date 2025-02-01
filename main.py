ba7036392bf6801106bc5bddf7ac26e42f0ca454
Abdel <abdessalam.aithaqi@gmail.com>
2025-02-01 18:07:47 -0500

    Refactor for readability

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-import time
 from object_tracking.object_tracker import ObjectTracker
 from utils.custom_logger import get_logger
 from face_display.face_display import FaceDisplay
-from hardware.cameras.imx500_camera import IMX500Camera
 from material_recognition import OpenAIClient
 
 # Initialize the logger
@@ -11,30 +8,21 @@ logger = get_logger(__name__)
 
 
 def main():
-
-    """
-    Main function to continuously scan for objects, capture images, 
-    and process them before returning to scanning.
-    """
     logger.info("Starting object detection system...")
-    
-    client = OpenAIClient(model="gpt-4o-mini")
-    
-    tracker = ObjectTracker(detection_distance=10)
-    
+    garbage_tracker = ObjectTracker(detection_distance=10)
+    openai = OpenAIClient(model="gpt-4o-mini")
 
     try:
         while True:
             # Scan for a new object
-            image_path = tracker.scan_for_new_object()
+            image_path = garbage_tracker.scan_for_new_object()
             
             if image_path:
                 logger.info(f"Captured image: {image_path}")
                 # Process the captured image
+                response_objects = openai.prompt(image_path)
                 
-                response_objects = client.prompt(filepath)
-                
-                detected_object = tracker.process_latest_image()
+                detected_object = garbage_tracker.process_latest_image()
                 logger.info(f"Detected Object: {detected_object}")
 
                 # Simulate further processing or display results
@@ -48,7 +36,7 @@ def main():
     except Exception as e:
         logger.critical(f"Unhandled exception: {e}")
     finally:
-        tracker.cleanup()
+        garbage_tracker.cleanup()
 
 if __name__ == "__main__":
     main()
