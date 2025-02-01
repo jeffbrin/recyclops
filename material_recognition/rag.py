0e5217ff5c7d9e80bb4370e219441c9339a10b02
Olivier Demers <oli.dmrs@dhcp-172-31-173-206.wireless.concordia.ca>
2025-02-01 14:14:34 -0500

    Created prompt for API

--- material_recognition/rag.py
+++ material_recognition/rag.py
@@ -4,27 +4,54 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
-# def encode_image(image_path):
-#     with open(image_path, "rb") as image_file:
-#         return base64.b64encode(image_file.read()).decode('utf-8')
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
 load_dotenv()
-API_KEY = os.environ.get("SECRET_KEY")
+API_KEY = os.environ.get("API_KEY")
 client = OpenAI(api_key=API_KEY)
 
+# Path to your image
+image_path = "path_to_your_image.jpg"
+
+# Getting the Base64 string
+base64_image = encode_image(image_path)
+
+Municipality = 'Montreal'
+
+
+
+prompt = f"""
+Instructions:
+- Analyze the provided image, which represents an item to be disposed of.
+- Each item may have multiple components (e.g., a cup and a lid).
+- Determine each visibile components that compose the image, for example, a cup and a lid.
+- For each component, determine what material it is.
+- If you can recognize the brand of the product and is certain of what recycling number it is, return the recycling number
+- For each material, Classify each component as Recycling, Garbage, Compost or Edge case, based on the waste management rules of {Municipality}.
+ 
+Questions:
+- For each component in the image, determine wheter it belongs to recycling, Garbage, Compost, or Edge Case.
+Format:
+- Return only a list of JSON objects, where each object maps a component name, the material and the disposable category. Only return
+the JSON objects absolutely no prose."""
+
 response = client.chat.completions.create(
-  model="gpt-4o",
-  messages=[
-    {
-      "role": "system",
-      "content": "Given the following SQL tables, your job is to write queries given a user’s request.\n    \n    CREATE TABLE Orders (\n      OrderID int,\n      CustomerID int,\n      OrderDate datetime,\n      OrderTime varchar(8),\n      PRIMARY KEY (OrderID)\n    );\n    \n    CREATE TABLE OrderDetails (\n      OrderDetailID int,\n      OrderID int,\n      ProductID int,\n      Quantity int,\n      PRIMARY KEY (OrderDetailID)\n    );\n    \n    CREATE TABLE Products (\n      ProductID int,\n      ProductName varchar(50),\n      Category varchar(50),\n      UnitPrice decimal(10, 2),\n      Stock int,\n      PRIMARY KEY (ProductID)\n    );\n    \n    CREATE TABLE Customers (\n      CustomerID int,\n      FirstName varchar(50),\n      LastName varchar(50),\n      Email varchar(100),\n      Phone varchar(20),\n      PRIMARY KEY (CustomerID)\n    );"
-    },
-    {
-      "role": "user",
-      "content": "Write a SQL query which computes the average total order value for all orders on 2023-04-01."
-    }
-  ],
-  temperature=1,
-  max_tokens=1024,
-  top_p=1
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": prompt,
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                },
+            ],
+        }
+    ],
 )
